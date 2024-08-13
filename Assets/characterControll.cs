014232e99c34c3962f0ac2578f7ea8b0609f0819
tokizg <145384085+tokizg@users.noreply.github.com>
2024-08-13 18:04:45 +0900

    create characterController.cs
    ## characterControll.cs
    * detect players input with Input.GetAxisRaw
    * move chararcter with ChararcterController.Move()

---
+++ Assets/characterControll.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class characterControll : MonoBehaviour
+{
+    CharacterController c_cCon;
+
+    [SerializeField]
+    float characterSpeed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c_cCon = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(xInput, 0, yInput).normalized;
+
+        if (direction.magnitude != 0f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 moveDir =
+                Quaternion.Euler(0f, targetAngle, 0f)
+                * Vector3.forward
+                * direction.magnitude
+                * characterSpeed;
+
+            c_cCon.Move(moveDir);
+        }
+    }
+}
